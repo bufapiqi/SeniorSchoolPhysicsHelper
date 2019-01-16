9e61deb17169242cc2b68b015c6c024151c4ffa9
bufapiqi <26716594@qq.com>
2019-01-16 17:20:34 +0800

    继续补充menu和menu_item

--- pygameEngine/menu.py
+++ pygameEngine/menu.py
@@ -6,17 +6,24 @@ from pygameEngine.menu_item import MenuItem
 
 class Menu:
 
-    def __init__(self, rect_s: tuple, rect_e: tuple, menu_item: list):
+    def __init__(self, rect_s: tuple, rect_e: tuple, menu_item: list, background=None):
         self.__rect_s = rect_s
         self.__rect_e = rect_e
+        self.__background = background
         if menu_item is None:
             self.__menu_item = []
         else:
             self.__menu_item = menu_item
 
-    def draw_menu(self):
-
-        pass
+    def draw_menu(self, screen):
+        screen.set_clip(self.__rect_s, self.__rect_e)
+        if type(self.__background) == str or type(self.__background) == tuple:
+            # 读图片
+            pass
+        else:
+            raise RuntimeError("background should be image_path or rgb_tuple !!!")
+        for i in self.__menu_item:
+            i.draw_item()  # todo 待完善
 
     def add_item(self, item: MenuItem):
         self.__menu_item.append(item)
@@ -24,3 +31,11 @@ class Menu:
     @property
     def menu_item(self):
         return self.__menu_item
+
+    @property
+    def rect_s(self):
+        return self.__rect_s
+
+    @property
+    def rect_e(self):
+        return self.__rect_e
--- pygameEngine/menu_item.py
+++ pygameEngine/menu_item.py
@@ -1,8 +1,53 @@
 """ pygame的渲染菜单选项的类，可以把该类添加到menu中，让menu进行集中渲染
 """
+# todo 待完善
 import pygame
+from pygame import image
+from util.Vector2D import Vec2d
 
 
 class MenuItem:
-    def __init__(self):
+
+    def __init__(self, up_image: image, down_image: image, relative_position: int):
+        self.__up_image = up_image
+        self.__down_image = down_image
+        self.__relative_position = relative_position
+
+    def draw_item(self, screen, menu_rect: tuple):
+        # caculate_position_with_menu()
+        # w, h = self.imageUp.get_size()
+        # x, y = self.position
+        #
+        # if self.isOver():
+        #     screen.blit(self.imageDown, (x-w/2,y-h/2))
+        # else:
+        #     screen.blit(self.imageUp, (x-w/2, y-h/2))
         pass
+
+    def is_click(self, rect: tuple):
+        point_x, point_y = pygame.mouse.get_pos()
+        point_a, point_b = MenuItem.caculate_position_with_menu(rect)
+        position = (Vec2d(point_b) - Vec2d(point_a)) / 2 + Vec2d(point_a)
+        x, y = position.x, position.y
+        w, h = self.__up_image.get_size()
+
+        in_x = x - w/2 < point_x < x + w/2
+        in_y = y - h/2 < point_y < y + h/2
+        return in_x and in_y
+
+    @staticmethod
+    def caculate_position_with_menu(rect: tuple):
+        # todo 从相对位置计算 在screen上的绝对位置
+        return (0, 0), (0, 0)
+
+    @property
+    def up_image(self):
+        return self.__up_image
+
+    @property
+    def down_image(self):
+        return self.__down_image
+
+    @property
+    def relative_position(self):
+        return self.__relative_position
